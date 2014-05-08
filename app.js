02961a7514650bcef4617b21800b00e4dba95f57
Wending Lu <wlu616@gmail.com>
2014-05-07 21:48:53 -0700

    dependencies

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var stream = require('./routes/stream');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/stream', stream);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
