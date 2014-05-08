722fde65bbff1cbd5a42dc173298df783ee196e6
Wending Lu <wlu616@gmail.com>
2014-05-08 01:31:30 -0700

    stream

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
 /* GET Userlist page. */
 router.get('/userlist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('usercollection');
-    collection.find({},{},function(e,docs){
-        res.render('userlist', {
-            "userlist" : docs
-        });
+  var db = req.db;
+  var collection = db.get('drawingcollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist', {
+      "userlist" : docs
     });
+  });
 });
 
 /* GET New User page. */
 router.get('/newuser', function(req, res) {
-    res.render('newuser', { title: 'Add New User' });
+  res.render('newuser', { title: 'Add New User' });
 });
 
 /* POST to Add User Service */
@@ -38,27 +38,27 @@ router.post('/adduser', function(req, res) {
     var user = req.body.user;
 
     // Set our collection
-    var collection = db.get('usercollection');
+    var collection = db.get('drawingcollection');
 
     // Submit to the DB
     collection.insert({
-        "title" : title,
-        "date" : date,
-        "image" : image,
-        "song" : song,
-        "user" : user
+      "title" : title,
+      "date"  : date,
+      "image" : image,
+      "song"  : song,
+      "user"  : user
 
     }, function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            // If it worked, set the header so the address bar doesn't still say /adduser
-            res.location("userlist");
-            // And forward to success page
-            res.redirect("userlist");
-        }
+      if (err) {
+          // If it failed, return error
+          res.send("There was a problem adding the information to the database.");
+      }
+      else {
+          // If it worked, set the header so the address bar doesn't still say /adduser
+          res.location("userlist");
+          // And forward to success page
+          res.redirect("userlist");
+      }
     });
 });
 
--- routes/stream.js
+++ routes/stream.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('stream', { title: 'Stream' });
+  var db = req.db;
+  var collection = db.get('drawingcollection');
+  collection.find({},{},function(e,docs){
+    console.log(docs);
+    res.render('stream', {
+      "drawings" : docs,
+      title: 'Stream'
+    });
+  });
 });
 
 module.exports = router;
