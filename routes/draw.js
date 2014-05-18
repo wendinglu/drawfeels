99e1e68a4bdcf7164d1faf3cae8925d244a18145
alt2014 <anhlt92@stanford.edu>
2014-05-18 03:20:05 -0700

    adapted for user and family members parameter; fixed circular image bug; enabled passing of the canvas and the selected recipients on submit

--- routes/draw.js
+++ routes/draw.js
@@ -4,8 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   var db = req.db;
+  var current_user = {'img': 'images/image1.jpg'}
+  var family_members = [{'img' :'images/image2.jpg'}, {'img': 'images/image3.jpg'}, {'img': 'images/image4.jpg'}, {'img': 'images/image5.jpg'}, {'img': 'images/image1.jpg'}];
   res.render('draw', {
-      title: 'Draw Something!'
+      title: 'Draw Something!',
+      "members": family_members,
+      "user": current_user
     });
 });
 
