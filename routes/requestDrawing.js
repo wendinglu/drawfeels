d7b51c7780ae92bd3c2f31aa931261362e91e5b8
alt2014 <anhlt92@stanford.edu>
2014-05-27 14:13:42 -0700

    integrated family/users into requestDrawing view, implemented modal for request in stream

--- routes/requestDrawing.js
+++ routes/requestDrawing.js
@@ -1,16 +1,39 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var familySchema = mongoose.model('familySchema');
-var drawingSchema = mongoose.model('drawingSchema');
-var requestSchema = mongoose.model('requestSchema');
+var Member = mongoose.model('memberSchema');
+var Family = mongoose.model('familySchema');
+var Drawing = mongoose.model('drawingSchema');
+var Request = mongoose.model('requestSchema');
 
+var renderMembers = function(id, callback) {
+  console.log("finding #{id}");
+  Family.findOne({_id: id}, function(err, family){
+    if (err)
+      callback(err, null)
+    else
+      Member.where({_id: {$in: family.members}}).exec(callback);
+  });
+}
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  var current_user = {'Name': 'Wending'}
-  var family_members = [{'img' :'images/image2.jpg'}, {'img': 'images/image3.jpg'}, {'img': 'images/image4.jpg'}, {'img': 'images/image5.jpg'}, {'img': 'images/image1.jpg'}];
-  res.render('requestDrawing', { title: 'Express', 'user': current_user, 'members': family_members });
+  var db = req.db;
+  var current_user = req.session.member;
+  
+  renderMembers(req.session.family._id, function(err, members){
+    if(err)
+      console.log (err);
+    else {
+      console.log ("Members found are:");
+      console.log (members);
+      res.render('requestDrawing', {
+        title: 'Request a Drawing!',
+        'members': members,
+        'user': current_user
+      });
+    }
+  });
 });
 
 router.post('/postRequest', function(req, res) {
