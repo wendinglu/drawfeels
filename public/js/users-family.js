6e789f283f90b09dbafd508203f3b41b670006e2
Wending Lu <wlu616@gmail.com>
2014-05-27 23:16:52 -0700

    slightly polished UI

--- public/js/users-family.js
+++ public/js/users-family.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
     debugger;
     window.location = 'chooseMember?id=' + $(this).data('id').replace(/\"/g, "");
   });
+
+  $('.profile-box').mouseenter(function() {
+    $(this).addClass('active');
+  });
+
+  $('.profile-box').mouseleave(function() {
+    $(this).removeClass('active');
+  });
 });
