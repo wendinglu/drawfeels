93e26f42655847e85fd2a4365761aa917fdd1f81
alt2014 <anhlt92@stanford.edu>
2014-05-28 07:35:54 -0700

    added a members lookup table as a passed in value to stream view; makes member lookup easier given their id

--- routes/stream.js
+++ routes/stream.js
@@ -3,26 +3,45 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Family = mongoose.model('familySchema');
 var Drawing = mongoose.model('drawingSchema');
-var Request = mongoose.model('requestSchema')
+var Request = mongoose.model('requestSchema');
+var Member = mongoose.model('memberSchema');
 
+var renderMembers = function(id, callback) {
+  console.log("finding #{id}");
+  Family.findOne({_id: id}, function(err, family){
+    if (err)
+      callback(err, null)
+    else
+      Member.where({_id: {$in: family.members}}).exec(callback);
+  });
+}
 /* GET home page. */
 router.get('/', function(req, res) {
-  //want a list 
 
   Request.find({to: req.session.member._id, active: req.session.member._id}, function(err, requestsFound){
     if (err) return console.log(err);
     console.log(requestsFound);
     Drawing.find( function ( err, drawingsFound){
       if (err) return console.log(err);
-      res.render( 'stream', {
-        title : 'Picture Stream',
-        drawings : drawingsFound,
-        requests: requestsFound
+      renderMembers(req.session.family._id, function(err, members){
+        if(err)
+          console.log (err);
+        else {
+          var membersTable = {}
+          members.forEach(function(value, index, arr){
+            membersTable[value._id] = value;
+          });
+          console.log(membersTable)
+          res.render( 'stream', {
+            title : 'Picture Stream',
+            drawings : drawingsFound,
+            requests: requestsFound,
+            membersTable: membersTable
+          });
+        }
       });
     });
   });
-
-  
 });
 
 module.exports = router;
